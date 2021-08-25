145d5945788550c31887d23d2ffa40e8e55a2253
Cameron Dudd <cameron.dudd@gmail.com>
2021-08-25 06:01:07 +0100

    proto V1

--- progress_documentation/proto_V1/device_V1.ino
+++ progress_documentation/proto_V1/device_V1.ino
@@ -1,5 +1,6 @@
 #include <LiquidCrystal.h>
 #include <Keypad.h>
+#include <string.h>
 
 
 
@@ -34,6 +35,21 @@ byte colPins[numCols]= {A0};
 Keypad myKeypad= Keypad(makeKeymap(keymap), rowPins, colPins, numRows, numCols);
 //-----
 
+void setup() {
+
+  startMillis = getTime();
+
+  pinMode(buttonPin, INPUT);
+  pinMode(rstPin, INPUT);
+
+  Serial.begin(9600);
+
+  lcd.begin(16, 2);
+  lcd.print("AIRSOC REPLICA");
+
+}
+
+
 int getTime() {
 
   int time = millis() / 1000;
@@ -42,15 +58,17 @@ int getTime() {
 }
 
 
-void setup() {
+void armedScreen(char currentKey, String keysPressed, int timeRemaining) {
 
-  startMillis = getTime();
-
-  pinMode(buttonPin, INPUT);
-  pinMode(rstPin, INPUT);
-
-  lcd.begin(16, 2);
-  lcd.print("AIRSOC REPLICA");
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(timeRemaining);
+  lcd.setCursor(15, 0);
+  lcd.print(currentKey);
+  lcd.setCursor(0, 1);
+  lcd.print(keysPressed);
+  delay(100);
+  return;
 
 }
 
@@ -62,30 +80,56 @@ void loop() {
   bool cleared = false;
 
   timeArmed = getTime();
+  char keysPressed[] = "00000000";
+  char keysNeeded[] = "12321321";
+  char keyNeeded;
+  int currentDigit = 0;
 
   while (armed and not exploded) {
-
+    armed = digitalRead(buttonPin);
     if (not cleared) {
       lcd.clear();
       cleared = true;
     }
 
-    armed = digitalRead(buttonPin);
-
     if (not exploded) {
       armed = digitalRead(buttonPin);
       timeRemaining = (countDownStart - (getTime() - timeArmed));
-      sprintf(countdown, "%d", timeRemaining);
-      lcd.setCursor(0, 1);
-      lcd.print(countdown);
+      
+      char keyPressed = myKeypad.getKey();
+      keyNeeded = keysNeeded[currentDigit];
+      if (keyPressed) {
+        keysPressed[currentDigit] = keyPressed;
+        currentDigit ++;
+      }
+      armedScreen(keyNeeded, keysPressed, timeRemaining);
+
+      int i;
+      int count = 0;
+
+      for (i=0; i<=7; i++) {
+        if (keysPressed[i] == keysNeeded[i]) {
+          count ++;
+        }
+      }
+
+      if (count == 8) {
+        lcd.clear();
+        lcd.setCursor(5, 0);
+        lcd.print("DEFUSED");
+        delay(2000);
+        exploded = true;
+      }
+
       if (timeRemaining == countDownEnd) {
         lcd.clear();
-        lcd.setCursor(0, 1);
+        lcd.setCursor(6, 0);
         lcd.print("BOOM");
         delay(2000);
         exploded = true;
       }
     }
+
   }
 
   if (reset) {
@@ -97,14 +141,5 @@ void loop() {
   lcd.print("AIRSOC REPLICA");
   lcd.setCursor(0, 1);
   lcd.print("UNARMED");
-
-  
-  char keypressed = myKeypad.getKey();
-  if (keypressed != NO_KEY) {
-    lcd.clear();
-    lcd.print(keypressed);
-    delay(1000);
-    lcd.clear();
-  }
 
 }
